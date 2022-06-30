ab538b400cec04b2d3357c4cf087cdad70f0c05b
akingo55 <am1157154@gmail.com>
2022-07-01 00:00:07 +0900

    fix indent

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,17 +1,17 @@
 let num = 1;
 
 function FizzBuzz(max_num){
-while (num <= max_num) {
-  if (num % 15 == 0){
-    console.log('FizzBuzz');
-  }else if (num % 5 == 0){
-    console.log('Buzz');
-  }else if (num % 3 == 0){
-    console.log('Fizz');
-  }else{
-    console.log(num);
-  }
-  num += 1;
+  while (num <= max_num) {
+    if (num % 15 == 0){
+      console.log('FizzBuzz');
+    }else if (num % 5 == 0){
+      console.log('Buzz');
+    }else if (num % 3 == 0){
+      console.log('Fizz');
+    }else{
+      console.log(num);
+    }
+    num += 1;
   }
 }
 
